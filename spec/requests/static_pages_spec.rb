a706ab8c6569b715abe5240907590dd51e1b981d
Yibo <roy.yangyb@gmail.com>
2013-02-16 19:51:02 +0800

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,20 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) {"YunnuY Soft"}
+  let(:base_path) {"/static_pages"}
+
   describe "Home page" do
     it "should have the content 'Sample App'" do 
-      visit '/static_pages/home'
+      visit "#{base_path}/home"
       page.should have_selector('h1', :text=> 'Sample App')
     end
  
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                    :text => "YunnuY Soft | Home")
+                    :text => "#{base_title} | Home")
     end
   end
 
@@ -24,7 +28,7 @@ describe "StaticPages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                    :text => "YunnuY Soft | Help")
+                    :text => "#{base_title} | Help")
     end
   end
 
@@ -38,8 +42,22 @@ describe "StaticPages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "YunnuY Soft | About Us")
+                    :text => "#{base_title} | About Us")
     end
+
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do 
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text=> 'Contact')
+    end
+  
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "#{base_title} | Contact")
+    end
+  end
 end
 #  describe "GET /static_pages" do
 
